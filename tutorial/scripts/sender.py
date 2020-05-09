537b6c8322a71e68dc1644b44ab9eaab5b95fc8c
Ewe Zu Lin <jamesewe.eed06@nctu.edu.tw>
2020-05-10 01:36:12 +0800

    add tutorial scrips

---
+++ tutorial/scripts/sender.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def sender():
+    pub = rospy.Publisher('chat', String, queue_size=10)
+    rospy.init_node('sender', anonymous=False)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(message)
+        pub.publish(message)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        sender()
+    except rospy.ROSInterruptException:
+        pass
